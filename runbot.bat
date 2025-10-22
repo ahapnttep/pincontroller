75306c8de9ac844bcd7dd2151eba940d2b5c00a4
Ana Ritter <anaclara.ritter@pm.me>
2025-10-22 10:04:24 -0300

    Delete runbot.bat

--- runbot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Vai para a pasta do bot
-cd "C:\Users\Ana\Desktop\Compiuter\Discord Bots\pin_controller\Pin Controller"
-
-REM Ativa a venv
-call .venv\Scripts\activate.bat
-
-REM Roda o bot em segundo plano sem abrir o terminal
-"C:\Users\Ana\Desktop\Compiuter\Discord Bots\pin_controller\Pin Controller\.venv\Scripts\pythonw.exe" pincontroller.py
